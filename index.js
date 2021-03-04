5d783ea057dbb782c559beb5745cc9d86cd6ae81
guopengwen <guopengwen@ymatou.com>
2021-03-04 19:11:01 +0800

    【title：initial】【content：工具类初始创建】

---
+++ index.js
@@ -0,0 +1,9 @@
+import CNPUtil from './util/CNPUtil';
+import CNPStrUtil from './util/CNPStrUtil';
+import CNPURLUtil from './util/CNPURLUtil';
+
+module.exports = {
+    CNPUtil,
+    CNPStrUtil,
+    CNPURLUtil
+}
